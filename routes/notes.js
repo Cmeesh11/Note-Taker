5d3b9afdb05f878d892e85d85a496e9f400fe5c4
Cmeesh11 <cartermeesh11@gmail.com>
2023-03-20 18:10:02 -0400

    C:/Program Files/Git/api/notes POST request now works correctly

--- routes/notes.js
+++ routes/notes.js
@@ -1,10 +1,17 @@
 const notesRouter = require("express").Router();
 const { join } = require("path");
 const fs = require("fs");
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
 notesRouter.get("/", (req, res) => {
-  res.json(join(__dirname, "..", "db", "db.json"));
+  // Reading file and returning the parsed data as a response
+  fs.readFile(join(__dirname, "..", "db", "db.json"), (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.json(JSON.parse(data));
+    }
+  });
 });
 
 notesRouter.post("/", (req, res) => {
@@ -14,20 +21,20 @@ notesRouter.post("/", (req, res) => {
       console.error(err);
     } else {
       // Parsing existing notes
-      const parsedNotes = JSON.parse(data);
+      const parsedNote = JSON.parse(data);
       // Giving new note an id
       req.body.id = uuidv4();
       // Appending new note to object
-      parsedNotes.push(req.body);
-      // Stringifying notes
-      let notes = JSON.stringify(parsedNotes, null);
-      fs.writeFile(join(__dirname, "..", "db", "db.json"), notes, (err) =>
-        err ? console.error(err) : console.log("Sucessfully added note")
+      parsedNote.push(req.body);
+      // Writing file with added note
+      fs.writeFile(
+        join(__dirname, "..", "db", "db.json"),
+        JSON.stringify(parsedNote, null, 4),
+        (err) =>
+          err ? res.json(err) : res.json("Sucessfully added note")
       );
     }
   });
-
-  req.body;
 });
 
 module.exports = notesRouter;
